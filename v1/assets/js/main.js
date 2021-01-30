c2c632ba6703ecfbe178d5168ccf16c9d6313e03
Matadantelinga <matadantelinga@192.168.1.3>
2021-01-31 00:43:50 +0700

    joss

--- v1/assets/js/main.js
+++ v1/assets/js/main.js
@@ -5,13 +5,13 @@
 	var mobileMenuOutsideClick = function() {
 
 		$(document).click(function (e) {
-	    var container = $("#fh5co-offcanvas, .js-fh5co-nav-toggle");
+	    var container = $("#mnt-offcanvas, .js-mnt-nav-toggle");
 	    if (!container.is(e.target) && container.has(e.target).length === 0) {
 
 	    	if ( $('body').hasClass('offcanvas') ) {
 
     			$('body').removeClass('offcanvas');
-    			$('.js-fh5co-nav-toggle').removeClass('active');
+    			$('.js-mnt-nav-toggle').removeClass('active');
 	    	}
 	    }
 		});
@@ -21,15 +21,15 @@
 
 	var offcanvasMenu = function() {
 
-		$('#page').prepend('<div id="fh5co-offcanvas" />');
-		$('#page').prepend('<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle fh5co-nav-white"><i></i></a>');
+		$('#page').prepend('<div id="mnt-offcanvas" />');
+		$('#page').prepend('<a href="#" class="js-mnt-nav-toggle mnt-nav-toggle mnt-nav-white"><i></i></a>');
 		var clone1 = $('.menu-1 > ul').clone();
-		$('#fh5co-offcanvas').append(clone1);
+		$('#mnt-offcanvas').append(clone1);
 		var clone2 = $('.menu-2 > ul').clone();
-		$('#fh5co-offcanvas').append(clone2);
+		$('#mnt-offcanvas').append(clone2);
 
-		$('#fh5co-offcanvas .has-dropdown').addClass('offcanvas-has-dropdown');
-		$('#fh5co-offcanvas')
+		$('#mnt-offcanvas .has-dropdown').addClass('offcanvas-has-dropdown');
+		$('#mnt-offcanvas')
 			.find('li')
 			.removeClass('has-dropdown');
 
@@ -56,7 +56,7 @@
 			if ( $('body').hasClass('offcanvas') ) {
 
     			$('body').removeClass('offcanvas');
-    			$('.js-fh5co-nav-toggle').removeClass('active');
+    			$('.js-mnt-nav-toggle').removeClass('active');
 				
 	    	}
 		});
@@ -65,7 +65,7 @@
 
 	var burgerMenu = function() {
 
-		$('body').on('click', '.js-fh5co-nav-toggle', function(event){
+		$('body').on('click', '.js-mnt-nav-toggle', function(event){
 			var $this = $(this);
 
 
@@ -193,7 +193,7 @@
 
 	// Loading page
 	var loaderPage = function() {
-		$(".fh5co-loader").fadeOut("slow");
+		$(".mnt-loader").fadeOut("slow");
 	};
 
 	var counter = function() {
@@ -205,8 +205,8 @@
 	};
 
 	var counterWayPoint = function() {
-		if ($('#fh5co-counter').length > 0 ) {
-			$('#fh5co-counter').waypoint( function( direction ) {
+		if ($('#mnt-counter').length > 0 ) {
+			$('#mnt-counter').waypoint( function( direction ) {
 										
 				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
 					setTimeout( counter , 400);					
